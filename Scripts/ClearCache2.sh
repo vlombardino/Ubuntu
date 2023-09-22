24f4c6cbfbd21dc2164d246e4fdbac5b05be4e5a
Vince Lombardino <67643138+vlombardino@users.noreply.github.com>
2023-09-22 13:44:30 -0700

    Update ClearCache2.sh

--- Scripts/ClearCache2.sh
+++ Scripts/ClearCache2.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 ############################################################
+### Notes
+############################################################
+### empty buffer and cache
+#sudo sh -c "free -h && sync && echo 3 > /proc/sys/vm/drop_caches && free -h"
+### free page cache
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
+### free dentries and inodes
+#sudo sh -c "echo 2 > /proc/sys/vm/drop_caches"
+### free page cache, dentries, and inodes
+#sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+
+############################################################
 ### VARIABLES
 ############################################################
 ### Free page cache, dentries, and inodes
