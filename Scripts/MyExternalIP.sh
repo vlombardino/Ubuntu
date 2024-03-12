b4a88d00a352a8c08f3ef89ae0f87251fd68ca67
Vince Lombardino <67643138+vlombardino@users.noreply.github.com>
2024-03-12 05:30:49 -0700

    Delete Scripts/MyExternalIP.sh

--- Scripts/MyExternalIP.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#wget
-wget -q -O - checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
-
-#lynx
-lynx -dump http://www.ip-adress.com/ip_tracer/?QRY=$1|grep address|egrep 'city|state|country'|awk '{print $3,$4,$5,$6,$7,$8}'|sed 's\ip address flag \\'|sed 's\My\\'
