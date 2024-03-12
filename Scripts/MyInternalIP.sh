9eecd83785abccaf127d97366958c300805166ff
Vince Lombardino <67643138+vlombardino@users.noreply.github.com>
2024-03-12 05:32:19 -0700

    Delete Scripts/MyInternalIP.sh

--- Scripts/MyInternalIP.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Requires net-tools
-#sudo apt install net-tools
-
-#http://askubuntu.com/questions/145012/how-can-i-find-my-public-ip-using-the-terminal
-ifconfig |grep -B1 "inet addr" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 ": " $3 }' echo "net: $(wget http://ipecho.net/plain -O - -q ; echo)"
